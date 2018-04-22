c99ea6d6182030fa0bf21d415b8745703f8c1c0f
Chirag Hingorani <chirag.hingorani@heavywater.solutions>
2018-04-21 20:55:06 -0700

    Adding signup files

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -9,24 +9,28 @@ import { LoginPage } from '../pages/login/login';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { SignupPage } from '../pages/signup/signup';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
     ListPage,
-    LoginPage
+    LoginPage,
+    SignupPage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    HttpClientModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
     ListPage,
-    LoginPage
+    LoginPage,
+    SignupPage
   ],
   providers: [
     StatusBar,
