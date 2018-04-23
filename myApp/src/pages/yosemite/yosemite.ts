5c8144bf41a4ea90cab42c9c66f5273607db11bc
Chirag Hingorani <chirag.hingorani@heavywater.solutions>
2018-04-23 12:28:11 -0700

    Adding Timer

--- myApp/src/pages/yosemite/yosemite.ts
+++ myApp/src/pages/yosemite/yosemite.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
@@ -7,6 +7,15 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
+ export interface CountdownTimer {
+   seconds: number;
+   secondsRemaining: number;
+   runTimer: boolean;
+   hasStarted: boolean;
+   hasFinished: boolean;
+   displayTime: string;
+ }
+
 
 @IonicPage()
 @Component({
@@ -14,12 +23,78 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'yosemite.html',
 })
 export class YosemitePage {
-
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
+  timeInSeconds: any= 30;
+  timer: CountdownTimer;
+
+  ngOnInit() {
+    this.initTimer();
+  }
+
+  hasFinished() {
+    return this.timer.hasFinished;
+  }
+
+  initTimer() {
+    if (!this.timeInSeconds) { this.timeInSeconds = 0; }
+
+    this.timer = <CountdownTimer>{
+      seconds: this.timeInSeconds,
+      runTimer: false,
+      hasStarted: false,
+      hasFinished: false,
+      secondsRemaining: this.timeInSeconds
+    };
+
+    this.timer.displayTime = this.getSecondsAsDigitalClock(this.timer.secondsRemaining);
+  }
+
+  startTimer() {
+    this.timer.hasStarted = true;
+    this.timer.runTimer = true;
+    this.timerTick();
+  }
+
+  pauseTimer() {
+    this.timer.runTimer = false;
+  }
+
+  resumeTimer() {
+    this.startTimer();
+  }
+
+  timerTick() {
+    setTimeout(() => {
+      if (!this.timer.runTimer) { return; }
+      this.timer.secondsRemaining--;
+      this.timer.displayTime = this.getSecondsAsDigitalClock(this.timer.secondsRemaining);
+      if (this.timer.secondsRemaining > 0) {
+        this.timerTick();
+      } else {
+        this.timer.hasFinished = true;
+      }
+    }, 1000);
+  }
+
+  getSecondsAsDigitalClock(inputSeconds: number) {
+    const secNum = parseInt(inputSeconds.toString(), 10); // don't forget the second param
+    const hours = Math.floor(secNum / 3600);
+    const minutes = Math.floor((secNum - (hours * 3600)) / 60);
+    const seconds = secNum - (hours * 3600) - (minutes * 60);
+    let hoursString = '';
+    let minutesString = '';
+    let secondsString = '';
+    hoursString = (hours < 10) ? '0' + hours : hours.toString();
+    minutesString = (minutes < 10) ? '0' + minutes : minutes.toString();
+    secondsString = (seconds < 10) ? '0' + seconds : seconds.toString();
+    return hoursString + ':' + minutesString + ':' + secondsString;
+  }
+
   ionViewDidLoad() {
-    console.log('ionViewDidLoad YosemitePage');
+      console.log('ionViewDidLoad YosemitePage');
+
   }
 
 }
