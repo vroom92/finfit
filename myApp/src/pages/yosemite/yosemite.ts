1d60e4de3a32f5a8d6625d31ae40c3b2c5da13d0
Varun Agrawal <varunagr@usc.edu>
2018-04-23 13:32:03 -0700

    Adding questions

--- myApp/src/pages/yosemite/yosemite.ts
+++ myApp/src/pages/yosemite/yosemite.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 /**
  * Generated class for the YosemitePage page.
@@ -7,6 +8,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
+ export interface Config {
+	banking: string;
+}
 
 @IonicPage()
 @Component({
@@ -14,12 +18,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'yosemite.html',
 })
 export class YosemitePage {
+  public questions: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public _HTTP : HttpClient) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad YosemitePage');
+    this._HTTP
+    .get<Config>('../../assets/data/questions.json')
+    .subscribe((data) =>
+    {
+      this.questions=data.banking;
+       console.log(this.questions[0].options);
+    });
   }
 
 }
